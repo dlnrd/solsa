dc3b952c314aba4da35299b7a910c56a22ff06bf
dlnrd <dylan.leonard@hotmail.co.uk>
2025-03-28 19:15:31 +0000

    Removed unnecessary declarations

--- tests/VariablePacking_test.go
+++ tests/VariablePacking_test.go
@@ -8,9 +8,9 @@ import (
 )
 
 func TestVariablePacking(t *testing.T) {
-	variables := []opt.Variable{opt.Variable{Index: 0, Size: 60}, opt.Variable{Index: 1, Size: 200}, opt.Variable{Index: 2, Size: 33}, opt.Variable{Index: 3, Size: 123}, opt.Variable{Index: 4, Size: 256}, opt.Variable{Index: 5, Size: 82}, opt.Variable{Index: 6, Size: 2}, opt.Variable{Index: 7, Size: 50}, opt.Variable{Index: 8, Size: 159}, opt.Variable{Index: 9, Size: 232}}
+	variables := []opt.Variable{{Index: 0, Size: 60}, {Index: 1, Size: 200}, {Index: 2, Size: 33}, {Index: 3, Size: 123}, {Index: 4, Size: 256}, {Index: 5, Size: 82}, {Index: 6, Size: 2}, {Index: 7, Size: 50}, {Index: 8, Size: 159}, {Index: 9, Size: 232}}
 
-	expected := [][]opt.Variable{[]opt.Variable{opt.Variable{Index: 4, Size: 256}}, []opt.Variable{opt.Variable{Index: 9, Size: 232}, opt.Variable{Index: 6, Size: 2}}, []opt.Variable{opt.Variable{Index: 1, Size: 200}, opt.Variable{Index: 7, Size: 50}}, []opt.Variable{opt.Variable{Index: 8, Size: 159}, opt.Variable{Index: 5, Size: 82}}, []opt.Variable{opt.Variable{Index: 3, Size: 123}, opt.Variable{Index: 0, Size: 60}, opt.Variable{Index: 2, Size: 33}}}
+	expected := [][]opt.Variable{{{Index: 4, Size: 256}}, {{Index: 9, Size: 232}, {Index: 6, Size: 2}}, {{Index: 1, Size: 200}, {Index: 7, Size: 50}}, {{Index: 8, Size: 159}, {Index: 5, Size: 82}}, {{Index: 3, Size: 123}, {Index: 0, Size: 60}, {Index: 2, Size: 33}}}
 
 	result := opt.VariablePacking(variables)
 	assert.Equal(t, expected, result)
